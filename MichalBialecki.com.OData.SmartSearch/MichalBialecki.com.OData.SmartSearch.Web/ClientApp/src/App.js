6eb1533c6e913aadf4f56f6976e6450c82d773e4
Michał Białecki <michal.bialecki@guestline.com>
2020-03-09 21:55:41 +0100

    Updated react to use hooks and fetch profiles

--- MichalBialecki.com.OData.SmartSearch/MichalBialecki.com.OData.SmartSearch.Web/ClientApp/src/App.js
+++ MichalBialecki.com.OData.SmartSearch/MichalBialecki.com.OData.SmartSearch.Web/ClientApp/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
+import FetchData from './components/FetchData';
 import { Counter } from './components/Counter';
 
 import './custom.css'
@@ -11,7 +11,7 @@ export default class App extends Component {
   static displayName = App.name;
 
   render () {
-    return (
+      return (
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
--- MichalBialecki.com.OData.SmartSearch/MichalBialecki.com.OData.SmartSearch.Web/ClientApp/src/components/FetchData.js
+++ MichalBialecki.com.OData.SmartSearch/MichalBialecki.com.OData.SmartSearch.Web/ClientApp/src/components/FetchData.js
@@ -1,59 +1,184 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export class FetchData extends Component {
-  static displayName = FetchData.name;
+export default () => {
 
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
-  }
+    const [profiles, setProfiles] = useState([]);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [city, setCity] = useState('');
 
-  componentDidMount() {
-    this.populateWeatherData();
-  }
+    useEffect(() => {
+        populateProfileData();
+    }, []);
 
-  static renderForecastsTable(forecasts) {
+    const handleFirstNameChange = (event) => {
+        setFirstName(event.target.value);
+        populateProfileData(event.target.value, lastName, city);
+    }
+
+    const handleLastNameChange = (event) => {
+        setLastName(event.target.value);
+        populateProfileData(firstName, event.target.value, city);
+    }
+
+    const handleCityChange = (event) => {
+        setLastName(event.target.value);
+        populateProfileData(firstName, lastName, event.target.value);
+    }
+
+    const populateProfileData = async (fn, ln, ci) => {
+
+        var url = 'odata/profiles?$top=25';
+
+        var queries = [];
+        if (fn !== undefined && fn !== '') {
+            queries.push('contains(FirstName,\'' + fn + '\')');
+        }
+
+        if (ln !== undefined && ln !== '') {
+            queries.push('contains(LastName,\'' + ln + '\')');
+        }
+
+        if (ci !== undefined && ci !== '') {
+            queries.push('contains(City,\'' + ci + '\')');
+        }
+
+        if (queries.length > 0) {
+            url += '&$filter=' + queries.join(' And ');
+        }
+        console.log('url', url);
+        const response = await fetch(url);
+        const data = await response.json();
+        setProfiles(data.value);
+    }
+
     return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+        <div>
+            <h1 id="tabelLabel">User profiles</h1>
+            <p>This component demonstrates fetching data from the server with OData.</p>
+            <form>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="firstname">Firstname</label>
+                        <input type="text" className="form-control" id="firstname" placeholder="John" onChange={handleFirstNameChange} />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="lastname">Lastname</label>
+                        <input type="text" className="form-control" id="lastname" placeholder="Wick" onChange={handleLastNameChange} />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="city">City</label>
+                    <input type="text" className="form-control" id="city" placeholder="Los Angeles" onChange={handleCityChange} />
+                </div>
+
+                <table className='table table-striped' aria-labelledby="tabelLabel">
+                    <thead>
+                        <tr>
+                            <th>FirstName</th>
+                            <th>LastName</th>
+                            <th>Email</th>
+                            <th>Street</th>
+                            <th>City</th>
+                            <th>PhoneNumber</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {profiles.map(profile =>
+                            <tr key={profile.Id}>
+                                <td>{profile.FirstName}</td>
+                                <td>{profile.LastName}</td>
+                                <td>{profile.Email}</td>
+                                <td>{profile.Street}</td>
+                                <td>{profile.City}</td>
+                                <td>{profile.PhoneNumber}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </form>
+        </div>
     );
-  }
-
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
-
-    return (
-      <div>
-        <h1 id="tabelLabel" >Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-        {contents}
-      </div>
-    );
-  }
-
-  async populateWeatherData() {
-    const response = await fetch('weatherforecast');
-    const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
-  }
 }
+
+
+//export class FetchData extends Component {
+//    static displayName = FetchData.name;
+
+//    constructor(props) {
+//        super(props);
+//        this.state = {
+//            profiles: [],
+//            firstName: '',
+//            lastName: '',
+//            city: '',
+//            loading: true
+//        };
+//    }
+
+//    componentDidMount() {
+//        this.populateProfileData();
+//    }
+
+//    static renderProfilesTable(profiles) {
+//        return (
+//            <form>
+//                <div class="form-row">
+//                    <div class="form-group col-md-6">
+//                        <label for="firstname">Firstname</label>
+//                        <input type="text" class="form-control" id="firstname" placeholder="John" value={this.state.firstName} />
+//                    </div>
+//                    <div class="form-group col-md-6">
+//                        <label for="lastname">Lastname</label>
+//                        <input type="text" class="form-control" id="lastname" placeholder="Wick" value={this.state.lastName} />
+//                    </div>
+//                </div>
+//                <div class="form-group">
+//                    <label for="city">City</label>
+//                    <input type="text" class="form-control" id="city" placeholder="Los Angeles" value={this.state.city} />
+//                </div>
+
+//                <table className='table table-striped' aria-labelledby="tabelLabel">
+//                    <thead>
+//                        <tr>
+//                            <th>FirstName</th>
+//                            <th>LastName</th>
+//                            <th>Email</th>
+//                            <th>Street</th>
+//                            <th>City</th>
+//                            <th>PhoneNumber</th>
+//                        </tr>
+//                    </thead>
+//                    <tbody>
+//                        {profiles.map(profile =>
+//                            <tr key={profile.Id}>
+//                                <td>{profile.FirstName}</td>
+//                                <td>{profile.LastName}</td>
+//                                <td>{profile.Email}</td>
+//                                <td>{profile.Street}</td>
+//                                <td>{profile.City}</td>
+//                                <td>{profile.PhoneNumber}</td>
+//                            </tr>
+//                        )}
+//                    </tbody>
+//                </table>
+//            </form>
+//        );
+//    }
+
+//    render() {
+//        let contents = this.state.loading
+//            ? <p><em>Loading...</em></p>
+//            : FetchData.renderProfilesTable(this.state.profiles);
+
+//        return (
+//            <div>
+//                <h1 id="tabelLabel" >User profiles</h1>
+//                <p>This component demonstrates fetching data from the server with OData.</p>
+//                {contents}
+//            </div>
+//        );
+//    }
+
+
+//}
